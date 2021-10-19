ae7dd9015f24e0f6f7c072aa5ff7947c27424b66
thanhnv011094 <84114660+thanhnv011094@users.noreply.github.com>
2021-10-19 17:09:26 +0700

    commit angular 8 demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { API_URL } from './constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo1';
+export class AppComponent implements OnInit {
+
+  title = API_URL;
+
+  ngOnInit(): void {
+    console.log(this.title);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFooterComponent,
+    MainBodyComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
